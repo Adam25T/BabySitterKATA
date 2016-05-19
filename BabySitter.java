5f0c0531b3cad4924937c89c85ad13802a24ba18
Adam25T <ajturner@owu.edu>
2016-05-18 23:37:22 -0400

    5th Test (PASSED)
    Added parameter and data member for bed time as well as method
    getBedTime() to retrieve myBedTime.
    Next Step: Parameter, data member, and getEndTime() method

--- BabySitter.java
+++ BabySitter.java
@@ -1,12 +1,18 @@
 
 public class BabySitter {
 	private Integer myStartTime;
+	private Integer myBedTime;
 	
 	public String getStartTime(){
 		return myStartTime.toString();
 	}
-	
+	public String getBedTime(){
+		return myBedTime.toString();
+	}
 	BabySitter(Integer startTime, Integer bedTime, Integer endTime){
 		myStartTime = startTime;
+		myBedTime = bedTime;
 	}
+	
+	
 }
