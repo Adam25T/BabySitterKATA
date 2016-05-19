83933dbe5371ee49fb6e16daa34ee2eef8f29802
Adam25T <ajturner@owu.edu>
2016-05-18 23:33:02 -0400

    Fourth Test (FAILED)
    The method getBedTime() does not exist
    Next Step: create getBedTime() and data member myBedTime

--- BabySitterTestCase.java
+++ BabySitterTestCase.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 public class BabySitterTestCase {
 
 	@Test
-	public void WhenBabyIsGivenAStartTimeofOneItReturnsThatOne() {
+	public void WhenBabySitterIsGivenAStartTimeofOneItReturnsThatOne() {
 		BabySitter babySitter = new BabySitter(1, 0, 0);
 		assertEquals("1", babySitter.getStartTime());
+	}
+
+	@Test
+	public void WhenBabySitterIsgivenABedTimeOfTwoItReturnsThatTwo() {
+		BabySitter babySitter = new BabySitter(1, 2, 0);
+		assertEquals("2", babySitter.getBedTime());
 	}
 
 }
